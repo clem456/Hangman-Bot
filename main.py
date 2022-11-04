b33008a7381e3e195faac040bf428978ddd7bf27
Clement <35793@ma-web.nl>
2022-11-04 11:28:18 +0100

    made code cleaner and added stuff into main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import nextcord
+import hanging
 from nextcord.ext import commands
 from languages import *
 
@@ -19,27 +20,40 @@ bot = commands.Bot(
 with open('url.txt') as file:
     generator_url = file.read()
 
-word = requests.get(generator_url).json()
-
 start_msg = "Welcome USER"
 start_cmd = "hangman"
 start_runnig = "USER is already playing hangman"
 
+guess_cmd = "guess"
+
 resposne1 = "You can choose language with command '!choose LANGUAGE'"
 
-playing = []
+playing = {}
 
 @bot.command()
 async def start(ctx, *, msg=None):
+    global playing
+    
     if not str(ctx.author.id) in playing:
         if msg.lower() == start_cmd:
             new_msg = start_msg.replace("USER", ctx.author.mention)
             new_msg += '\n'+resposne1
         
             for language in languages:
-                new_msg += '\n'+language
+                new_msg += '\n'+language.upper()
 
-            playing.append(str(ctx.author.id))
+            word = requests.get(generator_url).json()
+
+            playing[str(ctx.author.id)] = {
+                'lives' : 7,
+                'untranslated' : word[0],
+                'language' : None,
+                'translated' : None,
+                'hidden' : "",
+                'letters' : []
+            }
+
+            print("untranslated word is ", playing[str(ctx.author.id)]['untranslated'])
 
             return await ctx.send(new_msg)
     elif msg == None or msg.lower() != start_cmd:
@@ -54,6 +68,8 @@ async def start(ctx, *, msg=None):
 
 @bot.command()
 async def choose(ctx, *, msg=None):
+    global playing
+
     if not str(ctx.author.id) in playing:
         new_string = "USER hasn't started hangman yet or choose an invalid language"
 
@@ -71,7 +87,82 @@ async def choose(ctx, *, msg=None):
         return await ctx.send(new_msg)
 
     if msg.lower() in languages:
+        print("lan", msg.lower())
+
+        playing[str(ctx.author.id)]['language'] = msg.lower()
+        
+        taal = playing[str(ctx.author.id)]['language']
+
+        woord = playing[str(ctx.author.id)]['untranslated']
+        translated = None
+
+        if playing[str(ctx.author.id)]['language'] == "en":
+            playing[str(ctx.author.id)]['translated'] = woord
+
+            translated = playing[str(ctx.author.id)]['translated']
+        else:
+            translated = translators.google(woord, 'auto', taal)
+            
+            playing[str(ctx.author.id)]['translated'] = translated
+        
+        hiddenword = ""
+
+        for i in range(len(translated.strip())):
+            hiddenword += "🔴 "
+
+        playing[str(ctx.author.id)]['hidden'] = hiddenword
+
         await ctx.send(ctx.author.mention+" starting hangman in: "+msg.upper())
+        await ctx.send("guess word: "+playing[str(ctx.author.id)]['hidden'])
+
+@bot.command()
+async def guess(ctx, *, msg=None):
+    global playing
+
+    msg_strip = msg.strip()
+
+    if not str(ctx.author.id) in playing:
+        return
+    elif playing[str(ctx.author.id)]['language'] == None:
+        return await ctx.send(ctx.author.mention+" hasn't chosen a language yet\nuse command '!choose LANGUAGE'")
+    elif msg == None or msg.lower() in playing[str(ctx.author.id)]['letters']:
+        return await ctx.send(ctx.author.mention+" has already geussed letter: "+msg.lower())
+    elif len(msg_strip) != 1:
+        return await ctx.send(ctx.author.mention+" message either contains spaces or is not a letter")
+    
+    translated = playing[str(ctx.author.id)]['translated']
+    geussed_letters = playing[str(ctx.author.id)]['letters']
+
+    print(translated)
+    
+    if msg.lower() in translated.lower():
+        geussed_letters.append(msg.lower())
+
+        await ctx.send("letter "+msg.upper()+" is in word")
+    else:
+        playing[str(ctx.author.id)]['lives'] -= 1
+
+        lives = playing[str(ctx.author.id)]['lives']
+
+        geussed_letters.append(msg.lower())
+
+        await ctx.send(msg.upper()+" is not in word\n"+hanging.hanging[lives])
+    
+    if playing[str(ctx.author.id)]['lives'] == 0:
+        del playing[str(ctx.author.id)]
+
+        await ctx.send("your word was "+translated)
+
+@bot.command()
+async def steps(ctx):
+    intro_str = "Hallo, "+ctx.author.mention
+    intro_str += "\n1. To begin Hangman use command '!start hangman'"
+    intro_str += "\n2. Use command '!choose LANGUAGE' to choose your preffered language"
+    intro_str += "\n3. Use command '!guess LETTER' to guess a letter"
+    intro_str += "\n4. Now enjoy :)"
+    
+    await ctx.send(intro_str)
+    
 
 @bot.event
 async def on_ready():
